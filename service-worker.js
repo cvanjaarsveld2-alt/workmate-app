a8a70c7277a1cd8d976770cdcbf2eb1c7ab1a74e
cvanjaarsveld2-alt <cvanjaarsveld2@icloud.com>
2026-05-04 09:48:49 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "workmate-pwa-v1";
-const ASSETS = ["/", "/manifest.webmanifest"];
+const CACHE_NAME = "powermate-pwa-v2";
+const ASSETS = ["/", "/manifest.webmanifest", "/index.html"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
