7b44c5bcd82736b821263860ea7631b53a56085b
cvanjaarsveld2-alt <cvanjaarsveld2@icloud.com>
2026-07-12 17:55:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-a// ─── PowerMate App ────────────────────────────────────────────────────────────
+// ─── PowerMate App ────────────────────────────────────────────────────────────
 import React, { useEffect, useState, useCallback, useRef} from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
